7bfccbbea72529b2f240469a5b2ce4d4937a3ae0
youssouph-gano <youssouphgano@gmail.com>
2024-09-24 16:07:18 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -8,9 +8,9 @@ global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
 
-global.ownernumber = '2347063956321'  //creator number
-global.ownername = 'Precious Ayomide👑' //owner name
-global.botname = '𝕻𝕽𝕰𝖃𝖄 𝕭𝖀𝕾 𝕻3' //name of the bot
+global.ownernumber = '2217736005321'  //creator number
+global.ownername = 'DESTRUCTION😈' //owner name
+global.botname = '🤍 GÃNØ 🖤' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
